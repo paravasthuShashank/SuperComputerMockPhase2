8a051e101872609c35f1bb69c38bcfe1c28501ea
Paravasthu <shashankp96@gmail.com>
2020-05-26 23:54:57 +0530

    v1

--- application.py
+++ application.py
@@ -1,12 +1,13 @@
-import json
 import flask
-from flask import request, jsonify, Response
+from flask import Flask, Response, request, jsonify
+import json
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 app.config["SERVER_NAME"] = "127.0.0.1:1024"
-responseJsonDir = '../responseJsonResources/'
-responseHtmlDir = '../responseHtmlResources/'
+responseJsonDir = './responseJsonResources/'
+responseHtmlDir = './responseHtmlResources/'
+configurationFilePath = './configuration.json'
 
 def func():
     global json_object
@@ -18,7 +19,7 @@ def func():
         responseJsonFilePath = responseJsonDir + json_object[url]["responseJsonFileName"]
         responseJsonFile = open(responseJsonFilePath)
         responseJson = json.load(responseJsonFile)
-        responseHtmlFile.close()
+        responseJsonFile.close()
         response.set_data(str(responseJson))
 
     if("html" in json_object[url]):
@@ -45,7 +46,7 @@ def iterate_json(json_object):
         app.add_url_rule(url,response["endpoint"],func,methods=['GET','POST'])
 
 
-config_file = open("../configuration.json")
+config_file = open(configurationFilePath)
 json_object = json.load(config_file)
 config_file.close()
 iterate_json(json_object)
